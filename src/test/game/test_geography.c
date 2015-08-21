36dc94a6941ad00467fda9d69dc8e91f74f42d6f
Andy Sommerville <aksommerville@gmail.com>
2015-08-21 17:01:30 -0400

    Fixed 2 unreachable cells near San Jose CA.

--- src/test/game/test_geography.c
+++ src/test/game/test_geography.c
@@ -9,12 +9,19 @@
 
 CTM_UNIT_TEST(test_outer_banks_impassable) {
   CTM_ASSERT_CALL(ctm_grid_init(),"")
+
+  /* Outer Banks */
   CTM_ASSERT_NOT(ctm_location_is_vacant(232,158,0),"")
   CTM_ASSERT_NOT(ctm_location_is_vacant(234,155,0),"")
   CTM_ASSERT_NOT(ctm_location_is_vacant(234,154,0),"")
   CTM_ASSERT_NOT(ctm_location_is_vacant(235,154,0),"")
   CTM_ASSERT_NOT(ctm_location_is_vacant(235,153,0),"")
   CTM_ASSERT_NOT(ctm_location_is_vacant(235,152,0),"")
+
+  /* San Jose y=96,97 x=15,16 */
+  CTM_ASSERT_NOT(ctm_location_is_vacant(15,97,0),"")
+  CTM_ASSERT_NOT(ctm_location_is_vacant(16,96,0),"")
+  
   ctm_grid_quit();
   return 0;
 }
